5be666dfaab99d1502a74d9e0da054c9d70f83f7
Trevor Berninger <baladashman@protonmail.com>
2020-12-07 23:59:19 -0800

    tests pass. input crashes. I sleepy

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -1,6 +1,7 @@
 #![feature(str_split_once)]
 
 use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::io::Read;
 use std::path::PathBuf;
 use structopt::StructOpt;
@@ -13,9 +14,98 @@ struct Settings {
     part: String,
 }
 
-struct Passport(HashMap<String, String>);
+#[derive(Debug)]
+enum Units {
+    In(usize),
+    Cm(usize),
+}
+
+#[derive(Debug)]
+struct Passport {
+    byr: usize,
+    iyr: usize,
+    eyr: usize,
+    hgt: Units,
+    hcl: String,
+    ecl: String,
+    pid: String,
+}
 
 impl Passport {
+    fn valid(&self) -> bool {
+        if self.byr < 1920 || self.byr > 2002 {
+            return false;
+        }
+        if self.iyr < 2010 || self.iyr > 2020 {
+            return false;
+        }
+        if self.eyr < 2020 || self.eyr > 2030 {
+            return false;
+        }
+        match self.hgt {
+            Units::Cm(height) => {
+                if height < 150 || height > 192 {
+                    return false;
+                }
+            }
+            Units::In(height) => {
+                if height < 59 || height > 76 {
+                    return false;
+                }
+            }
+        }
+        if !self.hcl.starts_with('#') {
+            return false;
+        }
+        if usize::from_str_radix(&self.hcl[1..], 16).is_err() {
+            return false;
+        }
+        let valid_eye_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+        if !valid_eye_colors.iter().any(|&c| self.ecl.as_str() == c) {
+            return false;
+        }
+        if self.pid.len() != 9 {
+            return false;
+        }
+        if self.pid.parse::<usize>().is_err() {
+            return false;
+        }
+        true
+    }
+}
+
+impl TryFrom<PassportStrings> for Passport {
+    type Error = &'static str;
+
+    fn try_from(strings: PassportStrings) -> Result<Self, Self::Error> {
+        if !strings.valid() {
+            return Err("Missing values");
+        }
+        Ok(Self {
+            byr: strings.0["byr"].parse().unwrap(),
+            iyr: strings.0["iyr"].parse().unwrap(),
+            eyr: strings.0["eyr"].parse().unwrap(),
+            hgt: {
+                let s = &strings.0["hgt"];
+                let value = s[0..s.len() - 2].parse().unwrap();
+                if s.ends_with("in") {
+                    Units::In(value)
+                } else if s.ends_with("cm") {
+                    Units::Cm(value)
+                } else {
+                    return Err("Invalid height");
+                }
+            },
+            hcl: strings.0["hcl"].clone(),
+            ecl: strings.0["ecl"].clone(),
+            pid: strings.0["pid"].clone(),
+        })
+    }
+}
+
+struct PassportStrings(HashMap<String, String>);
+
+impl PassportStrings {
     fn valid(&self) -> bool {
         let required_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
         required_keys.iter().all(|&key| self.0.contains_key(key))
@@ -23,21 +113,25 @@ impl Passport {
 }
 
 fn part1(input: String) -> usize {
-    parse(input.to_string())
+    input_to_passport_strings(input.to_string())
         .iter()
         .filter(|passport| passport.valid())
         .count()
 }
 
-fn part2(_input: String) -> usize {
-    unimplemented!()
+fn part2(input: String) -> usize {
+    input_to_passport_strings(input.to_string())
+        .drain(..)
+        .filter_map(|passport_strings| Passport::try_from(passport_strings).ok())
+        .filter(|passport| passport.valid())
+        .count()
 }
 
-fn parse(input: String) -> Vec<Passport> {
+fn input_to_passport_strings(input: String) -> Vec<PassportStrings> {
     input
         .split("\n\n")
         .map(|passport| {
-            Passport(
+            PassportStrings(
                 passport
                     .replace('\n', " ")
                     .split_ascii_whitespace()
@@ -73,7 +167,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::part1;
+    use crate::{part1, part2};
 
     const TEST_INPUT: &'static str = r#"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
@@ -94,5 +188,33 @@ iyr:2011 ecl:brn hgt:59in"#; // TODO: split on empty lines, then build a diction
         assert_eq!(part1(TEST_INPUT.to_string()), 2);
     }
     #[test]
-    fn tes2() {}
+    fn tes2() {
+        let valid = r#"pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"#;
+        let invalid = r#"eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007"#;
+        assert_eq!(part2(valid.to_string()), 4);
+        assert_eq!(part2(invalid.to_string()), 0);
+    }
 }
